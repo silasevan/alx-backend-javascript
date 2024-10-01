632634c6030dbb6c075c573ef8ca28daf1288af3
silasevan <silasevan@gmail.com>
2024-10-01 18:25:32 +0100

    createReport

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,12 @@
+export default function createIteratorObject(report) {
+  const employees = [];
+
+  // Loop through each department in the report
+  for (const department in report.allEmployees) {
+    // Add each employee to the employees array
+    employees.push(...report.allEmployees[department]);
+  }
+
+  // Return an iterator for the employees array
+  return employees[Symbol.iterator]();
+}
