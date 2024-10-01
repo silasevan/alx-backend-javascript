1775687c319fc9efd1a01fe57ef2822768b1034c
silasevan <silasevan@gmail.com>
2024-10-01 16:50:58 +0100

    1-block-scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // Block-scoped, won't overwrite the outer task
-    const task2 = false; // Block-scoped, won't overwrite the outer task2
+    // The variables inside the block are unnecessary and can be removed.
   }
 
-  return [task, task2]; // Returns the outer task and task2
+  return [task, task2];
 }
